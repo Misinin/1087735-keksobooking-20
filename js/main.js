a54277bb715c1d2a8f1e85efbff3417365c42813
Misinin_Mikhail <mmisinin14@gmail.com>
2020-06-16 21:38:04 +0300

    Выполняет первую часть Доверяй но проверяй

--- js/main.js
+++ js/main.js
@@ -171,11 +171,9 @@ function renderPins() {
   return mapPins.appendChild(fragment);
 }
 
-renderPins();
-
 var offerTemplate = document.querySelector('#card').content;
 var offerItem = offerTemplate.querySelector('.map__card');
-var placeCardAdded = document.querySelector('.map');
+var mapBlock = document.querySelector('.map');
 
 /**
  * Наполняет контентом шаблон карточки предложения, отрисовывает ее и возвращает
@@ -303,7 +301,38 @@ function renderCardOffer(objectOffer) {
   return clonedOfferItem;
 }
 
-placeCardAdded.appendChild(renderCardOffer(adObjects[0]));
+// mapBlock.appendChild(renderCardOffer(adObjects[0]));
+
+var mainPin = document.querySelector('.map__pin--main');
+var adForm = document.querySelector('.ad-form');
+var formFieldsets = adForm.querySelectorAll('fieldset');
+var filterPins = document.querySelector('.map__filters').querySelectorAll('select');
+var rentalPricePerNight = document.querySelector('#price');
+var typeOfRentalHousing = document.querySelector('#type');
+var timeFields = document.querySelector('.ad-form__element--time');
+var addressField = document.querySelector('#address');
+var resetFormButton = document.querySelector('.ad-form__reset');
+var MinimumPrice = {
+  'bungalo': 0,
+  'flat': 1000,
+  'house': 5000,
+  'palace': 10000
+};
+var MainPinOffset = {
+  X: 36,
+  Y: 84
+};
+
+/**
+ *Устанавливает логическое значение атрибуту disabled
+ * @param {Object} fildsetArray - массив fildset'ов
+ * @param {boolean} state - логическое состояние атрибута disabled
+ */
+function setBooleanValueAttributeFieldset(fildsetArray, state) {
+  fildsetArray.forEach(function (itemFieldset) {
+    itemFieldset.disabled = state;
+  });
+}
 
 /**
  * Выполняет проверку нажатия клавиши и запускает переданную функцию
@@ -318,15 +347,39 @@ function isKeyPress(evt, key, selectedFunction) {
 }
 
 /**
- *Устанавливает логическое значение атрибуту disabled
- * @param {Object} fildsetArray - массив fildset'ов
- * @param {boolean} state - логическое состояние атрибута disabled
+ * Устанавливает минимальное значение поля 'Цена за ночь' в зависимости от
+ * выбранного типа жилья
  */
-function setActivateOrDisabledFormElements(fildsetArray, state) {
-  fildsetArray.forEach(function (itemFieldset) {
-    itemFieldset.disabled = state;
-  }
-  );
+function setPriceOfBuilding() {
+  var typeHousingValue = typeOfRentalHousing.value;
+  rentalPricePerNight.min = MinimumPrice[typeHousingValue];
+  rentalPricePerNight.placeholder = MinimumPrice[typeHousingValue];
+}
+
+/**
+* Устанавливает полученное значение соседнему полю
+* @param {Object} evt - объект события
+*/
+function setTheSameValue(evt) {
+  var indexOfSelectedItem = evt.target.selectedIndex;
+  var selectFields = timeFields.querySelectorAll('select');
+  selectFields.forEach(function (selectItem) {
+    selectItem.selectedIndex = indexOfSelectedItem;
+  });
+}
+
+/**
+ * Возвращает координаты элемента относительно документа
+ * @param {Object} elem - вычисляемый элемент
+ * @return {Object}
+ */
+function getCoords(elem) { // кроме IE8-
+  var box = elem.getBoundingClientRect();
+
+  return {
+    top: box.top + pageYOffset,
+    left: box.left + pageXOffset
+  };
 }
 
 /**
@@ -334,30 +387,65 @@ function setActivateOrDisabledFormElements(fildsetArray, state) {
  * для чтения
  */
 function setValueAddressField() {
-  document.querySelector('.map__pin--main').addEventListener('mouseup', function (evt) {
-    document.querySelector('#address').value = (evt.pageX - OFFSET_X_FORPIN) + ', ' +
-    (evt.pageX - OFFSET_Y_FORPIN);
-    document.querySelector('#address').readOnly = true;
-  });
+  addressField.value = (Math.round(getCoords(mainPin).top + MainPinOffset.X)) +
+  ', ' + (Math.round(getCoords(mainPin).left + MainPinOffset.Y));
 }
 
-var MinimumPrices = {
-  'bungalo': 0,
-  'flat': 1000,
-  'house': 5000,
-  'palace': 10000
-};
+var headline = adForm.querySelector('#title');
 
 /**
- * Устанавливает минимальное значение поля 'Цена за ночь' в зависимости от
- * выбранного типа жилья
+ * Выполняет валидацию заголовку по его длине
  */
-function setPriceOfBuilding() {
-  var buildingPrice = document.querySelector('#price');
-  var typeBuilding = document.querySelector('#type').value;
+function validateHeadline() {
+  var getMinLengthAttribute = headline.getAttribute('minlength');
+  var getMaxLengthAttribute = headline.getAttribute('maxlength');
+  var validity = headline.validity;
 
-  buildingPrice.min = MinimumPrices[typeBuilding];
-  buildingPrice.placeholder = MinimumPrices[typeBuilding];
+  if (validity.tooShort) {
+    headline.setCustomValidity('Длина заголовка не менее ' + getMinLengthAttribute + ' символов.');
+  } else if (validity.tooLong) {
+    headline.setCustomValidity('Длина заголовка не более ' + getMaxLengthAttribute + ' символов.');
+  } else if (validity.valueMissing) {
+    headline.setCustomValidity('Обязательное поле');
+  } else {
+    headline.setCustomValidity('');
+  }
+}
+
+/**
+ * Выполняет валидацию заголовку по его длине в режиме ввода
+ */
+function validateHeadlineInput() {
+  var HeadlineLength = {
+    MIN: 30,
+    MAX: 100
+  };
+
+  var valueLength = headline.value.length;
+  if (valueLength < HeadlineLength.MIN) {
+    headline.setCustomValidity('Ещё ' + (HeadlineLength.MIN - valueLength) + ' симв.');
+  } else if (valueLength > HeadlineLength.MAX) {
+    headline.setCustomValidity('Удалите еще' + (HeadlineLength.MAX - valueLength) + ' симв.');
+  } else {
+    headline.setCustomValidity('');
+  }
+}
+
+/**
+ * Выполняет валидацию поля 'Цена за ночь'
+ */
+function priceFieldValidation() {
+  var minPriceValue = rentalPricePerNight.getAttribute('min');
+  var maxPriceValue = rentalPricePerNight.getAttribute('max');
+  var validity = rentalPricePerNight.validity;
+
+  if (validity.rangeOverflow) {
+    rentalPricePerNight.setCustomValidity('Максимальная цена аренды ' + maxPriceValue);
+  } else if (validity.rangeUnderflow) {
+    rentalPricePerNight.setCustomValidity('Минимальная цена аренды ' + minPriceValue);
+  } else {
+    rentalPricePerNight.setCustomValidity('');
+  }
 }
 
 var roomNumberSelect = document.querySelector('#room_number');
@@ -369,89 +457,117 @@ var RoomsFeatures = {
   100: [0]
 };
 
+/**
+ * Выполняет проверку соответствия выбранного количества комнат и количества гостей
+ */
 function validationRoomsSelect() {
   var selectRoomsValue = roomNumberSelect.value;
   var selectGuestValue = capacityBuildingSelect.value;
-  var validityMessage = '';
+  var guestsArray = RoomsFeatures[selectRoomsValue];
 
-  if (RoomsFeatures[selectRoomsValue].indexOf(selectGuestValue) === -1) {
-    switch (selectRoomsValue) {
+  if (guestsArray.indexOf(selectGuestValue) === -1) {
+    switch (+selectRoomsValue) {
       case 1:
-        validityMessage = 'Одна комната для одного гостя';
+        if (selectGuestValue === '1') {
+          capacityBuildingSelect.setCustomValidity('');
+        } else {
+          capacityBuildingSelect.setCustomValidity('В одну комнату можно поселить только одного гостя.');
+        }
         break;
 
       case 2:
-        validityMessage = 'Две комнаты для одного или двух гостей';
+        if (selectGuestValue === '1' || selectGuestValue === '2') {
+          capacityBuildingSelect.setCustomValidity('');
+        } else {
+          capacityBuildingSelect.setCustomValidity('В две комнаты можно поселить не больше двух гостей.');
+        }
         break;
 
       case 3:
-        validityMessage = 'Две комнаты для трех, двух или гостя';
+        if (selectGuestValue === '1' || selectGuestValue === '2' || selectGuestValue === '3') {
+          capacityBuildingSelect.setCustomValidity('');
+        } else {
+          capacityBuildingSelect.setCustomValidity('В три комнаты можно поселить от 1го до 3х гостей.');
+        }
         break;
 
       case 100:
-        validityMessage = 'Не для гостей';
+        if (selectGuestValue === '0') {
+          capacityBuildingSelect.setCustomValidity('');
+        } else {
+          capacityBuildingSelect.setCustomValidity('Дворец не для гостей.');
+        }
         break;
     }
   }
-  capacityBuildingSelect.setCustomValidity(validityMessage);
 }
-
-var timeFields = document.querySelector('.ad-form__element--time');
 
 /**
-* Выполняет выбор поля option соседнего selecta, в зависимости от выбранного
-* ранее
-* @param {Object} evt - объект события
-*/
-function selectCheckInOutValue(evt) {
-  var indexOfSelectedItem = evt.target.selectedIndex;
-  var selectFields = timeFields.querySelectorAll('select');
-  selectFields.forEach(function (selectItem) {
-    selectItem.selectedIndex = indexOfSelectedItem;
-  });
+ * Выполняет валидацию формы
+ */
+function validateForm() {
+  headline.addEventListener('invalid', validateHeadline);
+  headline.addEventListener('input', validateHeadlineInput);
+  rentalPricePerNight.addEventListener('invalid', priceFieldValidation);
+  roomNumberSelect.addEventListener('invalid', validationRoomsSelect);
+  capacityBuildingSelect.addEventListener('invalid', validationRoomsSelect);
 }
 
-function setPageInactiveState() {
-  setActivateOrDisabledFormElements((document.querySelector('.ad-form')), true);
-  setActivateOrDisabledFormElements((document.querySelector('.map__filters')), true);
-  document.querySelector('.map__pin--main').tabIndex = '0';
-  document.querySelector('.map__pin--main').addEventListener('keydown', function (evt) {
-    isKeyPress(evt, 'Enter', setPageActivate);
-  });
+window.addEventListener('load', setPageToInactive);
+
+/**
+ * Устанавливает странице неактивное состояние
+ */
+function setPageToInactive() {
+  mapBlock.classList.add('map--faded');
+  adForm.classList.add('ad-form--disabled');
   document.querySelector('.map__pin--main').addEventListener('mousedown', function (evt) {
     if (evt.which === 1) {
       setPageActivate();
-      setValueAddressField();
     }
+  });
+  document.querySelector('.map__pin--main').addEventListener('keydown', function (evt) {
+    isKeyPress(evt, 'Enter', setPageActivate);
+  });
+  setBooleanValueAttributeFieldset(formFieldsets, true);
+  setBooleanValueAttributeFieldset(filterPins, true);
+  typeOfRentalHousing.removeEventListener('change', setPriceOfBuilding);
+  timeFields.removeEventListener('change', setTheSameValue);
+  resetFormButton.removeEventListener('click', function () {
+    setPageToInactive();
+    adForm.reset();
+  });
+  mainPin.addEventListener('mousedown', function (evt) {
+    setValueAddressField(evt);
   });
 }
 
 /**
- * Переводит страницу в активное состояние
+ * Устанавливает странице активное состояние
  */
 function setPageActivate() {
-  setActivateOrDisabledFormElements((document.querySelector('.ad-form')), false);
-  setActivateOrDisabledFormElements((document.querySelector('.map__filters')), false);
-  document.querySelector('.ad-form').classList.remove('ad-form--disabled');
-  document.querySelector('.map__filters').style.opacity = 1;
-  setPriceOfBuilding();
-  document.querySelector('#type').addEventListener('change', setPriceOfBuilding);
-
-  timeFields.addEventListener('change', selectCheckInOutValue);
+  mapBlock.classList.remove('map--faded');
+  adForm.classList.remove('ad-form--disabled');
+  setBooleanValueAttributeFieldset(formFieldsets, false);
+  setBooleanValueAttributeFieldset(filterPins, false);
+  renderPins();
+  document.querySelector('.map__pin--main').removeEventListener('mousedown', function (evt) {
+    if (evt.which === 1) {
+      setPageActivate();
+    }
+  });
+  document.querySelector('.map__pin--main').removeEventListener('keydown', function (evt) {
+    isKeyPress(evt, 'Enter', setPageActivate);
+  });
+  typeOfRentalHousing.addEventListener('change', setPriceOfBuilding);
+  timeFields.addEventListener('change', setTheSameValue);
+  resetFormButton.addEventListener('click', function () {
+    setPageToInactive();
+    adForm.reset();
+  });
+  validationRoomsSelect();
+  adForm.addEventListener('click', validateForm);
+  mainPin.removeEventListener('mouseup', function (evt) {
+    setValueAddressField(evt);
+  });
 }
-
-document.querySelector('#type').addEventListener('change', function () {
-  var buildingPrice = document.querySelector('#price');
-
-  if (buildingPrice.validity.rangeUnderflow) {
-    var min = document.querySelector('#price').getAttribute(min);
-    buildingPrice.setCustomValidity('Минимальное значение цены ' + min);
-  }
-
-  if (buildingPrice.validity.rangeOverflow) {
-    var max = document.querySelector('#price').getAttribute(max);
-    buildingPrice.setCustomValidity('Минимальное значение цены ' + max);
-  }
-});
-
-setPageInactiveState();
